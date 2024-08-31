83f637f9e2c7d018fe783b3866a6c35d577c84a0
Deadwalk <zgdmemail@gmail.com>
2024-09-01 01:06:26 +0800

    代码重构，去除冗余代码

--- app/agent.py
+++ app/agent.py
@@ -1,32 +1,18 @@
-# Chroma 原生操作
-import chromadb
-from chromadb import Client
-from chromadb import Settings
-from langchain_core.prompts import ChatPromptTemplate
-#引入Chroma向量数据库
-from langchain_chroma import Chroma
-#字符串格式化输出（解析器）
-from langchain_core.output_parsers import StrOutputParser
-#可运行对象（占位符）
-from langchain_core.runnables import RunnablePassthrough
-# 文档切分器
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-import pylibmagic
-import magic
-
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.prompts import SystemMessagePromptTemplate #系统消息模板
-from langchain_core.prompts import HumanMessagePromptTemplate #用户消息模板
-from langchain_core.prompts import AIMessagePromptTemplate #挖空模板，一般不用
-from langchain_core.prompts import ChatMessagePromptTemplate #通用消息模板
+import os
+import sys
+import logging
+import pprint
+from datetime import datetime
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage #三个消息
-
-from langchain_core.runnables.base import RunnableLambda
 from langchain_core.runnables.base import RunnableMap
-
 from langchain_community.llms.tongyi import Tongyi
 from langchain_community.chat_models import ChatTongyi
 from langchain_community.embeddings import DashScopeEmbeddings
+from langchain_community.utilities import SQLDatabase
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
+from langchain_core.messages import SystemMessage
+# 智能体Agent初始化（使用预构建的LangGraph Agent来构建）
+from langgraph.prebuilt import create_react_agent
 
 # llm大模型
 llm = Tongyi(model = "qwen-max",temperature=0.1,top_p=0.7,max_tokens=1024,api_key="sk-2be00aba82564503af9ca25d22cda9cd")
@@ -35,16 +21,19 @@ chat = ChatTongyi(model = "qwen-max",temperature=0.1,top_p=0.7,max_tokens=1024,a
 # embedding大模型
 embed = DashScopeEmbeddings(model="text-embedding-v3",dashscope_api_key="sk-2be00aba82564503af9ca25d22cda9cd")
 
-from langchain_community.utilities import SQLDatabase
-from langchain_community.agent_toolkits import SQLDatabaseToolkit
-from langchain_core.messages import SystemMessage
-# 智能体Agent初始化（使用预构建的LangGraph Agent来构建）
-from langgraph.prebuilt import create_react_agent
+# 配置日志
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+)
+
+# 将递归限制提高到100或更高
+# sys.setrecursionlimit(10)  
 
 # agent
-class Agent_sql():
+class Agent_SQL():
     
-    def __init__(self,path):
+    def __init__(self, path):
         """连接本地数据库"""
         self.db = SQLDatabase.from_uri(f"sqlite:///{path}")
         self.toolkit = SQLDatabaseToolkit(db=self.db, llm=llm)
@@ -63,21 +52,45 @@ class Agent_sql():
             To start you should ALWAYS look at the tables in the database to see what you can query.
             Do NOT skip this step.
             Then you should query the schema of the most relevant tables.
-            如果所有表中都没有查询到相关的信息，就停止查询，返回没有查询到结果即可"""
+            特别注意：
+            如果所有表中都没有查询到相关的信息，就停止查询，返回没有查询到结果即可。
+            如果查询过程中SQL语句有语法错误，减少查询量。"""
         
         self.system_message = SystemMessage(content=self.SQL_PREFIX)
         self.agent_executor = create_react_agent(chat, self.tools, state_modifier =self.system_message)
 
-    def get_result(self,input):
+    def get_chain(self):
+        """获取链"""
+        return self.agent_executor
+
+    def get_result(self, input):
         """查询 Agent"""
-        example_query = input#"请帮我查询出20210415日，建筑材料一级行业涨幅超过5%（不包含）的股票数量"
-        #"上海华铭智能终端设备股份有限公司的首发战略配售结果如何？"
         
+        example_query = input
+        logging.info(f"查询输入: {example_query}")
+
         events = self.agent_executor.stream(
             {"messages": [("user", example_query)]},
             stream_mode="values",
         )
+        
         result_list = []
-        for event in events:
-            result_list.append(event["messages"][-1].content)
-        return event["messages"][-1].content,result_list
+        try:
+            for event in events:
+                logging.info(pprint.pformat(event["messages"][-1].pretty_print()))
+                result_list.append(event["messages"][-1].content)
+        except Exception as e:
+            logging.error(f"处理事件时发生错误: {e}")
+        
+        final_result = event["messages"][-1].content if result_list else None
+        logging.info(f"最终结果: {final_result}")
+        return final_result, result_list
+    
+
+if __name__ == "__main__":
+    current_directory = os.getcwd()
+    sql_path = os.path.join(current_directory, "app/data/dataset/博金杯比赛数据.db")
+    agent = Agent_SQL(path=sql_path)
+    input = "请帮我查询出20210415日，建筑材料一级行业涨幅超过5%（不包含）的股票数量"
+    result,result_list = agent.get_result(input)
+    print(result)

3133d3777ac68ad662543d7fd6a86358acd00d32
Deadwalk <zgdmemail@gmail.com>
2024-08-31 21:00:26 +0800

    修改导入的shell脚本

--- import_pdf.sh
+++ import_pdf.sh
@@ -6,4 +6,4 @@ CHROMA_SERVER_PORT=8000       # chromdb 端口号
 
 # 调用 pdf_processor.py
 echo "Running pdf_processor.py with server IP: $CHROMA_SERVER_IP and port: $CHROMA_SERVER_PORT"
-python app/rag/pdf_processor.py "$CHROMA_SERVER_IP" "$CHROMA_SERVER_PORT"
+python app/rag/pdf_processor.py --host "$CHROMA_SERVER_IP" --port "$CHROMA_SERVER_PORT"

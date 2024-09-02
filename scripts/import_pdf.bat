80422fb8913924590fb3ba09c0dcff8e4f018e74
Deadwalk <zgdmemail@gmail.com>
2024-09-03 02:59:47 +0800

    修改导入脚本

--- scripts/import_pdf.bat
+++ scripts/import_pdf.bat
@@ -1,7 +1,7 @@
 @echo off
-REM 配置服务器 IP 和端口
-SET CHROMA_SERVER_IP=localhost
-SET CHROMA_SERVER_PORT=8000
+@REM REM 配置服务器 IP 和端口
+@REM SET CHROMA_SERVER_IP=localhost
+@REM SET CHROMA_SERVER_PORT=8000
 
 REM 获取当前脚本所在目录
 SET SCRIPT_DIR=%~dp0
@@ -10,6 +10,6 @@ REM 设置 Python 脚本的路径
 SET PYTHON_SCRIPT=%SCRIPT_DIR%..\app\rag\pdf_processor.py
 
 REM 调用 pdf_processor.py
-echo Running pdf_processor.py with server IP: %CHROMA_SERVER_IP% and port: %CHROMA_SERVER_PORT%
-python "%PYTHON_SCRIPT%" --host "%CHROMA_SERVER_IP%" --port "%CHROMA_SERVER_PORT%"
+echo Running pdf_processor.py ...
+python "%PYTHON_SCRIPT%" 
 pause
